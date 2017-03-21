0c5e6a8ad27f9c05270a8988efb6be25513ee5c2
NullArray <NullArray@users.noreply.github.com>
2017-03-21 02:44:40 +0100

    Added blessings for term colors.

--- dorknet.py
+++ dorknet.py
@@ -10,10 +10,14 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+from blessings import Terminal
+
+t = Terminal()
+
 
 # Check for args, print logo and usage
 if not len(sys.argv[1:]):
-    print """
+    print t.cyan("""
  ____          _   _____     _   
 |    \ ___ ___| |_|   | |___| |_ 
 |  |  | . |  _| '_| | | | -_|  _|
@@ -27,7 +31,7 @@ To display all options available use -h or --help.
 
 Example:
 DorkNet.py -h
-DorkNet.py -d inurl:show.php?id= --verbose\n"""
+DorkNet.py -d inurl:show.php?id= --verbose\n""")
     
     sys.exit(0)
 
@@ -37,54 +41,54 @@ parser = argparse.ArgumentParser(description="Use this script and dorks to find 
 group = parser.add_mutually_exclusive_group()
 group.add_argument("-d", "--dork", help="		specify the dork you wish to use\n")
 group.add_argument("-l", "--list", help="		specify path to list with dorks\n")
-parser.add_argument("-v", "--verbose", action="store_true", help="	toggle verbosity\n")
+parser.add_argument("-v", "--verbose", action="store_true", help="		toggle verbosity\n")
 args = parser.parse_args()
 
 dork_list = []
 
 # Dork list processing
 if args.list:
-	print "\n[+]Reading in list from: " + args.list + "\n\n"	
+	print "\n[" + t.green("+") + "]Reading in list from: " + args.list + "\n\n"	
 	try:
 		with open(args.list, "r") as ins:
 			for line in ins:
 				dork_list.append(line)
 				
 				if args.verbose == True:
-					print "[~]" + line 
+					print "[" + t.magenta("~") + "]" + line 
 				
 	except IOError as e:
-		print "\n[!]Could not read dork list"
+		print "\n[" + t.red("!") + "]Could not read dork list"
 		if args.verbose == True:
 			print "\nAn IO Error was raised with the following error message: "
-			print "\n %s" % e
+			print "\n %s" % (e)
             
 else:
     dork_list.append(args.dork)
 
 
 
-print "\nWould you like DorkNet to proxy its connection to the search engine?"
+print "\n[" + t.green("+") + "]Would you like DorkNet to proxy it's connection to the search engine?"
 query = raw_input("[Y]es/[N]o: ")
 
 if query == 'y':
-	IP = raw_input("\nPlease enter the proxy host IP: ")
-	PORT = raw_input("\nPlease enter the proxy port: ")
+	IP = raw_input("\n[" + t.green("+") + "]Please enter the proxy host IP: ")
+	PORT = raw_input("\n[" + t.green("+") + "]Please enter the proxy port: ")
 	set_proxy = True
 elif query == 'n':
-	print "\n[+]Establishing unproxied connection..."
+	print "\n[" + t.green("+") + "]Establishing unproxied connection...\n"
 	set_proxy = False
 else:
-	print "\n[!]Unhandled option, defaulting to unproxied connection..."
+	print "\n[" + t.red("!") + "]Unhandled option, defaulting to unproxied connection..."
 	set_proxy = False
 
 
 # Web Driver Proxy
 def proxy(PROXY_HOST,PROXY_PORT):
 	fp = webdriver.FirefoxProfile()
-	print "[+]Proxy host set to: " + PROXY_HOST
-	print "[+]Proxy port set to: " + PROXY_PORT
-	print "\n[+]Establishing connection..."
+	print "[" + t.green("+") + "]Proxy host set to: " + PROXY_HOST
+	print "[" + t.green("+") + "]Proxy port set to: " + PROXY_PORT
+	print "\n[" + t.green("+") + "]Establishing connection..."
 	fp.set_preference("network.proxy.type", 1)
 	fp.set_preference("network.proxy.http",PROXY_HOST)
 	fp.set_preference("network.proxy.http_port",int(PROXY_PORT))
@@ -106,13 +110,13 @@ def search():
 		try:
 			driver.get("http://google.com")
 		except Exception as e:
-			print "\n[!]A connection could not be established"
+			print "\n[" + t.red("!") + "]A connection could not be established"
 			if args.verbose == True:
 				print "An error was raised with the following error message: "
-				print "\n %s" % e
-			break
-			driver.quit()
-			sys.exit(0)
+				print "\n %s" % (e)
+				break
+				driver.quit()
+				sys.exit(0)
 			
 		assert "Google" in driver.title
 		for items in dork_list:
@@ -120,19 +124,19 @@ def search():
 			elem.clear()
 			elem.send_keys(items)
 			elem.send_keys(Keys.RETURN)
-			time.sleep(1)
+			time.sleep(1.5)
 			
 			try:
 				WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.CLASS_NAME, "r")))
 			except Exception as e:
 				driver.quit()
-				print "\n[!]Detecting page source elements failed/timed out.\n"
+				print "\n[" + t.red("!") + "]Detecting page source elements failed/timed out.\n"
 				
 				if args.verbose == True:
 					print "An error was raised with the following error message: "
-					print "\n %s" % e
+					print "\n %s" % (e)
 				
-				time.sleep(1.3)
+				time.sleep(1)
 				continue	
 				
 				
@@ -153,11 +157,11 @@ proc_one = search()
 with open("results.log", "w") as outfile:
 	for item in proc_one:
 		outfile.write("%s\n" % item)
-		
+
 if args.verbose == True:	
 	with open("results.log", "r") as infile:
 		for line in infile:
-			print "[~]" + line
+			print "[" + t.magenta("~") + "]" + line
 		
 
-print "\n\nDone. Results have been saved to a textfile, in the current directory as %s for further processing." % outfile
+print "\n\n[" + t.green("+") + "]Done. Results have been saved to a textfile, in the current directory as %s for further processing.\n" % outfile
